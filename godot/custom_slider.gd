95c651ba86d637333f0b44cc2b4e0344037b1a6a
KyoheiAbc <38806568+KyoheiAbc@users.noreply.github.com>
2025-09-19 14:40:32 +0900

    Update custom_slider.gd

--- godot/custom_slider.gd
+++ godot/custom_slider.gd
@@ -1,22 +1,14 @@
-class_name CustomSlider
-extends HSlider
+class CustomHSlider extends HSlider:
+	func _init(text: String):
+		size = Vector2(300, 300)
 
-func _init(text: String, min_value: float, max_value: float, step: float, initial_value: float = 0):
-	self.value = initial_value
-	self.min_value = min_value
-	self.max_value = max_value
-	self.step = step
-	self.size = Vector2(400, 240)
+		var label = Label.new()
+		add_child(label)
+		label.size = size
+		label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+		label.set_autowrap_mode(TextServer.AutowrapMode.AUTOWRAP_WORD)
+		label.add_theme_font_size_override("font_size", 64)
 
-	label = Label.new()
-	label.size = Vector2(800, 240)
-	label.position = Vector2(-800 / 4, 0)
-	label.text = text + ": " + str(int(self.value))
-	label.add_theme_font_size_override("font_size", 50)
-	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-	add_child(label)
-
-
-	self.value_changed.connect(func(value: float) -> void:
-		label.text = text + ": " + str(int(value))
-	)
+		value_changed.connect(func(value):
+			label.text = text + str(int(value))
+		)
